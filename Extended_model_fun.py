0e0c8fbfc970de37e64b395eb2d95af51e48e21c
albertbrinckerolson <alboarddk@gmail.com>
2021-04-15 15:09:28 +0200

    test

---
+++ Extended_model_fun.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 15 13:31:45 2021
+
+@author: alboa
+"""
+
+def derivative_expanded(X, mp):
+    # *** Description ***
+    # Computes the derivative of X using model parameters
+
+    # ************* Input *************
+    #
+    # X : State vector
+        # S1 : susceptible at risk
+        # S2 : susceptible non risk
+        # I1 : regular infected
+        # I2 : ICU infected
+        # I3 : respirator infected
+        # R1 : regular recovered
+        # R2 : vacinated
+        # R3 : dead
+        
+    # mp : model parameters
+        # beta1 : Infection rate parameter in group at risk
+        # beta2 : Infection rate parameter in group not at risk
+        # gamma1 : Rate of recovery for infected
+        # gamma2 : Rate of recovery for ICU
+        # gamma3 : Rate of recovery for respirator
+        # t1 : Vaccination rate in group at risk
+        # t2 : Vaccination rate in group not at risk
+        # t3 : Vaccination rate of infected group
+        # t4 : Vaccination rate of recovered group
+        # theta1 : Death rate at ICU
+        # theta2 : Death rate in respirator
+        # phi1 : Rate of ICU from infected
+        # phi2 : Rate of respirator from ICU
+        # N : Population
+    # ************* Output *************
+    #
+    # dX : derivative of state vector X. 
+ 
+    # Extract data
+    beta1, beta2, gamma, t1, t2, t3, t4, theta1, theta2, phi1, phi2, N = mp
+    S1, S2, I1, I2, I3, R1, R2, R3 = X
+
+    dX = [
+        -(beta1 * (I1 / N) + t1)*S1, #dS1/dt
+        -(beta2 * (I1 / N) + t2)*S2, #dS2/dt
+        beta1 *(I1 / N) * S1 + beta2 * (I1 / N) * S2 - (gamma + t3 + phi1) * I1, #dI1/dt
+        phi1 * I1 - (gamma + theta1 + phi2) * I2, #dI2/dt 
+        phi2 * I2 - (gamma + theta2)* I3, #dI3/dt
+        gamma * (I1 + I2 + I3) - t4 * R1, #dR1/dt
+        t1 * S1 + t2 * S2 + t3 * I1 + t4 * R1, #dR2/dt
+        theta1 * I1 + theta2 * I2, #dR3/dt
+        
+    ]
+
+    return dX
---
+++ Vac_prog.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 30 11:12:16 2021
+
+@author: alboa
+
+
+"""
+
+import pandas as pd
+import numpy as np
+from get_data import*
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+
+# ***** Description *****
+#
+# Computes an estimate of daily activated vaccines, and compares with actual data
+# From ssi using get_data.py.
+#
+# ***** End *****
+
+
+# Read constant data from excel file.
+location = "C:/Users/alboa/Documents/GitHub/covid-19-modelling/data/Vaccine prognoser (jan-juli).xlsx"
+df = pd.read_excel(location,header=1)
+
+# Format relevant columns to np.arrays 
+daily_phizer = np.array([df[df.columns[8]]]).transpose()
+daily_moderna = np.array([df[df.columns[9]]]).transpose()
+daily_astra = np.array([df[df.columns[10]]]).transpose()
+daily_johnson = np.array([df[df.columns[11]]]).transpose()
+daily_andre = np.array([df[df.columns[12]]]).transpose()
+
+# Adds offset to the effect of vaccine, corresponding tothe number of weeks from delivery to fully vaccinated. 
+# value for 'others' category is not known, so its set to 4. 
+offsets = [3,4,int((4+9)/2),1, 4]#value for others category is not determined, set to 4. 
+
+# Adds zeros corresponding to the offset, so the effect of each vaccine is consistent with
+# vacination time (first data point is still 4th of january )
+phizer_active = np.append(np.zeros(offsets[0]*7),daily_phizer)
+moderna_active = np.append(np.zeros(offsets[1]*7),daily_moderna)
+astra_active = np.append(np.zeros(offsets[2]*7),daily_astra)
+johnson_active = np.append(np.zeros(offsets[3]*7),daily_johnson)
+andre_active = np.append(np.zeros(offsets[4]*7),daily_andre)
+
+
+
+max_points=max(len(phizer_active),len(moderna_active),len(astra_active),len(johnson_active),len(andre_active))
+
+
+# Extend with zeros so sizes are compatible
+max_points=max(len(phizer_active),len(moderna_active),len(astra_active),len(johnson_active),len(andre_active))
+phizer_active = np.append(phizer_active,np.zeros(max_points-len(phizer_active)))
+moderna_active = np.append(moderna_active,np.zeros(max_points-len(moderna_active)))
+astra_active = np.append(astra_active,np.zeros(max_points-len(astra_active)))
+johnson_active = np.append(johnson_active,np.zeros(max_points-len(johnson_active)))
+andre_active = np.append(andre_active,np.zeros(max_points-len(andre_active)))
+
+# total active vaccines (with individual offsets)
+Total_active = phizer_active + moderna_active + astra_active + johnson_active + andre_active
+
+# import vaccination data from ssi using get_data.py
+vac_df = vaccine_dict['FaerdigVacc_daekning_DK_prdag']
+faerdig_vac_daglig = np.array([vac_df[vac_df.columns[1]]]).transpose()
+
+# (Optional) remove zeros for clearer plot.
+remove = True
+if remove == True:
+    phizer_active[phizer_active==0] = 'nan'
+    moderna_active[moderna_active==0] = 'nan'
+    astra_active[astra_active==0] = 'nan'
+    johnson_active[johnson_active==0] = 'nan'
+    andre_active[andre_active==0] = 'nan'
+
+######### Generate plots ########
+
+t = np.linspace(0,max_points,max_points)
+
+plt.figure(0)
+plt.plot(t[0:len(phizer_active)],phizer_active)
+plt.plot(t[0:len(moderna_active)],moderna_active)
+plt.plot(t[0:len(astra_active)],astra_active)
+plt.plot(t[0:len(johnson_active)],johnson_active)
+plt.plot(t[0:len(andre_active)],andre_active)
+plt.plot(t[0:len(Total_active)],Total_active,':',linewidth=2,color='b')
+sns.despine()
+
+plt.title("Daily activated vaccines")
+plt.legend(["Phizer","Moderna","AstraZeneca","Johnson & Johnson","Others","Total"])
+plt.xlabel("days since 04/01/2021")
+plt.ylabel("Active vaccinations")
+
+# plots from 4th of january to last known observation of march 
+
+vac_data_points = len(faerdig_vac_daglig)
+t_vac_data = np.linspace(10,vac_data_points+9,vac_data_points)
+plt.figure(1)
+plt.plot(t,Total_active)
+plt.plot(t_vac_data,faerdig_vac_daglig)
+plt.title("Daily activated vaccines")
+plt.legend(["Planned","Actual"])
+plt.xlabel("days since 04/01/2021")
+plt.ylabel("Active vaccinations")
+sns.despine()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
