e6ec0529ebef35919cf63f7de8d74cd932f49b37
albertbrinckerolson <alboarddk@gmail.com>
2021-04-23 15:52:49 +0200

    Test of vaccine simulation

--- expanded_ivp_funcs.py
+++ expanded_ivp_funcs.py
@@ -6,15 +6,16 @@ Created on Thu Apr 15 13:31:45 2021
 """
 
 
-def derivative_expanded(X, mp):
+def derivative_expanded(X, mp, t):
     # *** Description ***
     # Computes the derivative of X using model parameters
 
     # ************* Input *************
     #
+    # t : added vaccinations 
+    #
     # X : State vector
-    # S1 : susceptible at risk
-    # S2 : susceptible non risk
+    # S : susceptible
     # I1 : regular infected
     # I2 : ICU infected
     # I3 : respirator infected
@@ -23,12 +24,10 @@ def derivative_expanded(X, mp):
     # R3 : dead
 
     # mp : model parameters
-    # beta1 : Infection rate parameter in group at risk
-    # beta2 : Infection rate parameter in group not at risk
+    # beta : Infection rate parameter 
     # gamma1 : Rate of recovery for infected
     # gamma2 : Rate of recovery for ICU
     # gamma3 : Rate of recovery for respirator
-    # t : Vaccination rate
     # theta1 : Death rate at ICU
     # theta2 : Death rate in respirator
     # phi1 : Rate of ICU from infected
@@ -39,18 +38,17 @@ def derivative_expanded(X, mp):
     # dX : derivative of state vector X. 
 
     # Extract data
-    beta1, beta2, gamma1, gamma2, gamma3, t, theta1, theta2, phi1, phi2, N = mp
-    S1, S2, I1, I2, I3, R1, R2, R3 = X
+    beta, gamma1, gamma2, gamma3, theta1, theta2, phi1, phi2, N = mp
+    S, I1, I2, I3, R1, R2, R3 = X
 
     dX = [
-        -(beta1 * (I1 / N) + t * (1 / (S1 + S2 + I1 + R2))) * S1,  # dS1/dt
-        -(beta2 * (I1 / N) + t * (1 / (S1 + S2 + I1 + R2))) * S2,  # dS2/dt
-        beta1 * (I1 / N) * S1 + beta2 * (I1 / N) * S2 - (gamma1 + phi1) * I1 - t * (1 / (S1 + S2 + I1 + R2)) * I1,
+        -((beta * I1)/ N + (t / (S + I1 + R1))) * S,  # dS/dt
+        (beta * I1 / N) * S - (gamma1 + phi1 +(t / (S + I1 + R1))) * I1,
         # dI1/dt
         phi1 * I1 - (gamma2 + theta1 + phi2) * I2,  # dI2/dt
         phi2 * I2 - (gamma3 + theta2) * I3,  # dI3/dt
-        gamma1 * I1 + gamma2 * I2 + gamma3 * I3 - t * (1 / (S1 + S2 + I1 + R2)) * R2,  # dR1/dt
-        t,  # dR2/dt
+        gamma1 * I1 + gamma2 * I2 + gamma3 * I3 - (t /(S + I1 + R1)) * R2,  # dR1/dt
+        t, # dR2/dt
         theta1 * I1 + theta2 * I2,  # dR3/dt
 
     ]
@@ -61,7 +59,9 @@ def derivative_expanded(X, mp):
 def RK4(
         X_k: list,  # Values of the expanded SIR at time t_k
         mp: list,  # Model parameters [beta, gamma, N]
-        stepsize: float = 0.1  # t_kp1 - t_k
+        t: int, # added vacinations
+        stepsize: float = 1# t_kp1 - t_k
+        
 ):
     # *** Description ***
     # Uses Rung Kutta 4 to solve ODE system numerically.
@@ -71,10 +71,10 @@ def RK4(
 
     N = range(len(X_k))
 
-    K_1 = derivative_expanded(X_k, mp)
-    K_2 = derivative_expanded([X_k[i] + 1 / 2 * stepsize * K_1[i] for i in N], mp)
-    K_3 = derivative_expanded([X_k[i] + 1 / 2 * stepsize * K_2[i] for i in N], mp)
-    K_4 = derivative_expanded([X_k[i] + stepsize * K_3[i] for i in N], mp)
+    K_1 = derivative_expanded(X_k, mp,t)
+    K_2 = derivative_expanded([X_k[i] + 1 / 2 * stepsize * K_1[i] for i in N], mp,t)
+    K_3 = derivative_expanded([X_k[i] + 1 / 2 * stepsize * K_2[i] for i in N], mp,t)
+    K_4 = derivative_expanded([X_k[i] + stepsize * K_3[i] for i in N], mp,t)
 
     X_kp1 = [X_k[i] + stepsize / 6 * (K_1[i] + 2 * (K_2[i] + K_3[i]) + K_4[i]) for i in N]
 
@@ -84,8 +84,9 @@ def RK4(
 def simulateSIR(
         X_0: list,  # Initial values of SIR [S_0, I_0, R_0]
         mp: list,  # Model parameters [beta, gamma, N]
+        T: list, # Total added vaccinations
         simtime: int = 100,  # How many timeunits into the future that should be simulated
-        stepsize: float = 0.1,  # t_kp1 - t_k
+        stepsize: float = 1,  # t_kp1 - t_k
         method=RK4  # Numerical method to be used [function]
 ):
     # *** Description ***
@@ -100,6 +101,6 @@ def simulateSIR(
     t = [i * stepsize for i in range(int(simtime / stepsize) + 1)]
 
     for i in range(int(simtime / stepsize)):
-        SIR.append(method(SIR[i], mp, stepsize))
+        SIR.append(method(SIR[i], mp,T[i], stepsize))
 
     return t, SIR
--- test_expanded_model.py
+++ test_expanded_model.py
@@ -6,11 +6,14 @@ Created on Thu Apr 15 15:22:09 2021
 """
 import matplotlib.pyplot as plt
 import expanded_ivp_funcs as e_ivp
+import numpy as np
+
+# Import added vaccine data
+Activated_vaccines = np.loadtxt('vac_data_kalender_14_04_2021.csv')
 
 simdays = 100
 # X : State vector
-# S1 : susceptible at risk
-# S2 : susceptible non risk
+# S : susceptible at risk
 # I1 : regular infected
 # I2 : ICU infected
 # I3 : respirator infected
@@ -18,8 +21,7 @@ simdays = 100
 # R2 : vaccinated
 # R3 : dead
 
-X_0 = [(6 * 10 ** 6 - 800000) * 0.2,
-       (6 * 10 ** 6 - 800000) * 0.8,
+X_0 = [(6 * 10 ** 6 - 800000),
        30000,
        400,
        300,
@@ -27,23 +29,19 @@ X_0 = [(6 * 10 ** 6 - 800000) * 0.2,
        500000,
        700]
 # mp : model parameters
-# beta1 : Infection rate parameter in group at risk
-# beta2 : Infection rate parameter in group not at risk
+# beta : Infection rate parameter
 # gamma1 : Rate of recovery for infected
 # gamma2 : Rate of recovery for ICU
 # gamma3 : Rate of recovery for respirator
-# t : Vaccination rate
 # theta1 : Death rate at ICU
 # theta2 : Death rate in respirator
 # phi1 : Rate to ICU from infected
 # phi2 : Rate to respirator from ICU
 # N : Population
-mp = [0.15,
-      0.22,
+mp = [0.22,
       (1 / 7),
       (1 / 20),
       (1 / 20),
-      15000,
       0.2 * (1 / 20),
       0.2 * (1 / 20),
       0.001,
@@ -53,14 +51,16 @@ mp = [0.15,
 t, State_vec = e_ivp.simulateSIR(
     X_0=X_0,
     mp=mp,
+    T = Activated_vaccines[30:130],
     simtime=simdays,
+    stepsize=1,
     method=e_ivp.RK4
 )
 
 plt.plot(t, State_vec)
 plt.title("Out of butt parameters")
-plt.legend(["S1", "S2", "I1", "I2", "I3", "R1", "R2 (vac)", "R3"])
+plt.legend(["S", "I1", "I2", "I3", "R1", "R2 (vac)", "R3"])
 
 plt.ylabel("Number of people")
-plt.ylim([0, 6 * 10 ** 3])
+plt.ylim([0, 6 * 10 ** 5])
 plt.show()
