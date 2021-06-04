e76d150d0f585c731b3e07974144a4515ba4af30
albertbrinckerolson <alboarddk@gmail.com>
2021-06-04 14:54:37 +0200

    PID controller added

--- expanded_ivp_funcs.py
+++ expanded_ivp_funcs.py
@@ -4,6 +4,7 @@ Created on Thu Apr 15 13:31:45 2021
 
 @author: alboa
 """
+import math
 
 
 def derivative_expanded(X, mp, t):
@@ -43,8 +44,7 @@ def derivative_expanded(X, mp, t):
 
     dX = [
         -((beta * I1)/ N + (t / (S + I1 + R1))) * S,  # dS/dt
-        (beta * I1 / N) * S - (gamma1 + phi1 +(t / (S + I1 + R1))) * I1,
-        # dI1/dt
+        (beta * I1 / N) * S - (gamma1 + phi1 +(t / (S + I1 + R1))) * I1,# dI1/dt
         phi1 * I1 - (gamma2 + theta1 + phi2) * I2,  # dI2/dt
         phi2 * I2 - (gamma3 + theta2) * I3,  # dI3/dt
         gamma1 * I1 + gamma2 * I2 + gamma3 * I3 - (t /(S + I1 + R1)) * R2,  # dR1/dt
@@ -80,7 +80,25 @@ def RK4(
 
     return X_kp1
 
+def PID_cont(X,mp,d,e_total,e_prev,K):
+    I2_hat = 1000
+    I3_hat = 1000
+    e = max(X[2]-I2_hat , X[3]-I3_hat)
+    e_total = e_total + e
+    
+    if d % 7 == 0:
+        try:
+            mp[0] = mp[0] * (0.8+0.4/(1+math.exp(-((K[0]* e  ) + K[1]*e_total + K[2]*(e-e_prev)))))
+        except: OverflowError
 
+    if mp[0] < 0:
+        mp[0] = 0
+    if mp[0] > 0.24:
+        mp[0] = 0.24
+
+    return mp[0],e,e_total
+        
+   
 def simulateSIR(
         X_0: list,  # Initial values of SIR [S_0, I_0, R_0]
         mp: list,  # Model parameters [beta, gamma, N]
@@ -102,5 +120,40 @@ def simulateSIR(
 
     for i in range(int(simtime / stepsize)):
         SIR.append(method(SIR[i], mp,T[i], stepsize))
+        
 
     return t, SIR
+
+def simulateSIR_PID(
+        X_0: list,  # Initial values of SIR [S_0, I_0, R_0]
+        mp: list,  # Model parameters [beta, gamma, N]
+        T: list, # Total added vaccinations
+        K: list, # parameters for penalty function
+        simtime: int = 100,  # How many timeunits into the future that should be simulated
+        stepsize: float = 1,  # t_kp1 - t_k
+        method=RK4  # Numerical method to be used [function]
+       
+):
+    # *** Description ***
+    # Simulate SIR-model.
+
+    # *** Output ***
+    # t [list]:             All points in time simulated
+    # SIR [nested list]:    Values of SIR at all points in time t
+
+    SIR = [X_0]
+
+    t = [i * stepsize for i in range(int(simtime / stepsize) + 1)]
+    e_total = 0
+    e_prev = 0
+    error_vals = []
+    beta_vals = []
+    for i in range(int(simtime / stepsize)):
+        mp[0],e_prev,e_total = PID_cont(SIR[i],mp,i,e_total,e_prev,K)
+        error_vals.append(e_prev)
+        beta_vals.append(mp[0])
+        SIR.append(method(SIR[i], mp,T[i], stepsize))
+        
+
+    return t, SIR, beta_vals,error_vals
+
--- test_expanded_model.py
+++ test_expanded_model.py
@@ -45,7 +45,7 @@ mp = [0.22,
       0.2 * (1 / 20),
       0.2 * (1 / 20),
       0.001,
-      0.3,
+      0.5,
       (6 * 10 ** 6)]
 
 t, State_vec = e_ivp.simulateSIR(
@@ -65,6 +65,10 @@ I3 = []
 R1 = []
 R2 = []
 R3 = []
+
+I2_hat = 1000
+I3_hat = 1000
+error_vals1 = []
 for i in range(0,len(t)):
     temp = State_vec[i]
     S.append(temp[0])
@@ -74,6 +78,9 @@ for i in range(0,len(t)):
     R1.append(temp[4])
     R2.append(temp[5])
     R3.append(temp[6])
+
+    e = max(temp[2]-I2_hat,0)+max(temp[3]-I3_hat,0)
+    error_vals1.append(e)
 
 plt.stackplot(t,S,I1,I2,I3,R1,R2,R3,labels = ["S", "I1", "I2", "I3", "R1", "R2 (vac)", "R3"])
 plt.title("Stacked area")
@@ -99,3 +106,54 @@ plt.legend(["I1", "I2", "I3", "R3"])
 plt.ylabel("Number of people")
 plt.ylim([0, 6 * 10 ** 5])
 plt.show()
+
+plt.plot(t,I2,I3)
+plt.title("Intensive and respirator")
+plt.legend([ "I2: intensive", "I3: Respirator"])
+
+plt.show()
+#%% PID control simulation 
+
+
+t, State_vec,beta_vals,error_vals = e_ivp.simulateSIR_PID(
+    X_0=X_0,
+    mp=mp,
+    T = Activated_vaccines[30:130],
+    K = [-1,-1/1000,-10],
+    simtime=simdays,
+    stepsize=1,
+    method=e_ivp.RK4
+)
+S_PID = [] 
+I1_PID = []
+I2_PID = []
+I3_PID = []
+R1_PID = []
+R2_PID = []
+R3_PID = []
+
+for i in range(0,len(t)):
+    temp = State_vec[i]
+    S_PID.append(temp[0])
+    I1_PID.append(temp[1])
+    I2_PID.append(temp[2])
+    I3_PID.append(temp[3])
+    R1_PID.append(temp[4])
+    R2_PID.append(temp[5])
+    R3_PID.append(temp[6])
+
+
+plt.plot(t,error_vals1,error_vals)
+plt.title("Errors (number of people above threshold)")
+plt.legend([ "Error with out PID", ""])
+plt.show()
+
+I2_threshold = [1000 for i in range(101)]
+I3_threshold = [1000 for i in range(101)]
+
+fig, axs = plt.subplots(2)
+axs[0].plot(t,I2,t,I3,t,I2_PID,t,I3_PID,t,I2_threshold,t,I3_threshold)
+axs[0].legend(["I2","I3","I2 with PID", "I3 with PID" ,"I2 threshold","I3 threshold"])
+beta_vals.append(beta_vals[-1])
+axs[1].plot(t,beta_vals)
+axs[1].legend("Beta Values")
