dcfe9423c54f1ebdc74cec2fd56e7395b2cefe40
albertbrinckerolson <alboarddk@gmail.com>
2021-06-09 09:45:27 +0200

    asd

--- main_expanded.py
+++ main_expanded.py
@@ -5,3 +5,16 @@ Created on Wed Jun  9 09:33:45 2021
 @author: alboa
 """
 
+import get_data as gd
+import matplotlib.pyplot as plt
+import time
+import datetime as dt
+import pandas as pd
+import numpy as np
+
+# Get data
+s1 = pd.to_datetime('2020-01-27') #start of data
+s2 = pd.to_datetime('2021-01-01') #start of simulation
+sim_days = 21
+
+I = gd.infect_dict['Test_pos_over_time'][s1 : s2 + dt.timedelta(days=sim_days)]
