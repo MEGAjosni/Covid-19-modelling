7399d753b0cf77b3f67e2b32e13d023736bcdd60
MarcusGalea <s194336@student.dtu.dk>
2021-06-10 14:05:35 +0200

    small changes

--- test_expanded_model.py
+++ test_expanded_model.py
@@ -17,8 +17,8 @@ simdays = 100
 # X : State vector
 # S : susceptible at risk
 # I1 : regular infected
-# I2 : ICU infected
-# I3 : respirator infected
+# I2 : Hospitalised
+# I3 : In ICU
 # R1 : regular recovered
 # R2 : vaccinated
 # R3 : dead
@@ -46,7 +46,6 @@ mp = [0.22,
       (1 / 7),
       (1 / 20),
       (1 / 20),
-      0.2 * (1 / 20),
       0.2 * (1 / 20),
       0.001,
       0.5,
@@ -113,7 +112,7 @@ plt.title(" infected and dead ")
 plt.legend(["I1", "I2", "I3", "R3"])
 
 plt.ylabel("Number of people")
-plt.ylim([0, 6 * 10 ** 5])
+plt.ylim([0, 1*10**6])
 plt.show()
 
 plt.plot(t,I2,I3)
