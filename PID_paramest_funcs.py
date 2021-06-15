9453d70e9fd2674a5276c2b53e40f4cd71b123b1
albertbrinckerolson <alboarddk@gmail.com>
2021-06-15 15:56:50 +0200

    asd

--- PID_paramest_funcs.py
+++ PID_paramest_funcs.py
@@ -21,9 +21,10 @@ import Data_prep_4_expanded as dp4e
 def estimate_params_expanded_PID(
         X_0,
         data,
-        mp,  # Known model parameters [beta,gamma1, gamma2, gamma3, theta, phi1, phi2]
+        mp, # Known model parameters [gamma1, gamma2, gamma3, theta, phi1, phi2]
+        beta_initial,
         simdays,
-        precision=2
+        precision,
 ):
     best_beta = -math.inf 
     best_params=[0,0,0]
@@ -31,9 +32,9 @@ def estimate_params_expanded_PID(
     for i in range(len(data['R2'])):
         T[(i*10):(i*10)+10] = data['R2'][i]/10
     for k in tqdm(range(precision)):
-            Kp = np.linspace(best_params[0] - 10 / (10 ** k), best_params[0] + 1 / (100 ** k), 21)
-            Ki = np.linspace(best_params[1] - 10 / (10 ** k), best_params[1] + 1 / (100 ** k), 21)
-            Kd = np.linspace(best_params[2] - 10 / (10 ** k), best_params[2] + 1 / (100 ** k), 21)
+            Kp = np.linspace(best_params[0] - 100 / (10 ** k), best_params[0] + 10 / (100 ** k), 21)
+            Ki = np.linspace(best_params[1] - 100 / (10 ** k), best_params[1] + 10 / (100 ** k), 21)
+            Kd = np.linspace(best_params[2] - 100 / (10 ** k), best_params[2] + 10 / (100 ** k), 21)
     
             for comb in itertools.product(Kp,Ki,Kd):
                 params = list(comb)
@@ -41,6 +42,7 @@ def estimate_params_expanded_PID(
                     X_0=X_0,
                     mp=mp,
                     T = T,
+                    beta_initial = beta_initial,
                     K = params,
                     simtime=simdays,
                     stepsize=0.1,
@@ -49,10 +51,10 @@ def estimate_params_expanded_PID(
                 )
 
                 if max(error_vals) <= 0:
-                    if min(beta_vals) > best_beta:
+                    if min(beta_vals[1:]) > best_beta:
                         best_params = params
-                        best_beta = min(beta_vals)
-                        
+                        best_beta = min(beta_vals[1:])
+            print("\n Current best params: ", best_params)        
     return best_beta, best_params
                     
                     
@@ -62,7 +64,7 @@ def estimate_params_expanded_PID(
                                 
 
 start_day = '2020-12-01'  # start day
-simdays = 100
+simdays = 50
 overshoot = 7
 beta,phi1,phi2 = [0.195738, 0.010765, 0.002307]
 gamma1 = 1/7
@@ -83,7 +85,7 @@ data = dp4e.Create_dataframe(
     forecast=False
 )
 
-mp = [beta, gamma1, gamma2, gamma3, theta, phi1, phi2]
+mp = [gamma1, gamma2, gamma3, theta, phi1, phi2]
 
 T = np.zeros(len(data['R2'])*10)
 for i in range(len(data['R2'])):
@@ -96,8 +98,9 @@ worst_case_beta , opt_params = estimate_params_expanded_PID(
     X_0=data.loc[t0 - overshoot].to_numpy(copy=True),
     data = data,
     simdays = simdays,
+    beta_initial = beta,
     mp=mp,
-    precision=4
+    precision=3
     )
 
 t, State_vec,beta_vals,error_vals = e_ivp.simulateSIR_PID(
@@ -105,6 +108,7 @@ t, State_vec,beta_vals,error_vals = e_ivp.simulateSIR_PID(
                     mp=mp,
                     T = T,
                     K = opt_params,
+                    beta_initial = beta,
                     simtime=simdays,
                     stepsize=0.1,
                     method=e_ivp.RK4
@@ -116,5 +120,4 @@ for i in range(len(t)):
     ICU.append(State_vec[i][3])
     
 plt.plot(t,ICU,t,np.ones(len(State_vec))*322)
-plt.bar()
-plt.plot(t,beta_vals)
+plt.plot(range(len(beta_vals)),beta_vals)
